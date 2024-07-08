c04c852a3ada2f4e23c1a6df2c9164cd3cbf6978
Your Name <you@example.com>
2024-07-08 22:12:01 +0530

    first commit

---
+++ SpringBootQuizAppApplicationTests/src/main/java/com/example/SpringBootQuizAppApplicationTests/Repository/QuestionRepo.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootQuizAppApplicationTests.Repository;
+
+
+
+import com.example.SpringBootQuizAppApplicationTests.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface QuestionRepo extends JpaRepository<Question, Integer> {
+
+}
---
+++ SpringBootQuizAppApplicationTests/src/test/java/com/example/SpringBootQuizAppApplicationTests/SpringBootQuizAppApplicationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootQuizAppApplicationTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuizAppApplicationTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
